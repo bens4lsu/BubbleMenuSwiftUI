941fb4a41f4108ca77d62c0a9aec84530e16c390
Ben Schultz <bens4lsu@gmail.com>
2019-11-13 15:48:13 -0600

    Initial Commit

---
+++ BubbleMenuSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BubbleMenuSwiftUI
+//
+//  Created by Ben Schultz on 11/13/19.
+//  Copyright © 2019 com.concordbusinessservicesllc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
